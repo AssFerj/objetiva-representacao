386359490cc515edab93be9dfe0c8c95a035cdce
AssFerj <assisjuniorcam@gmail.com>
2025-04-13 17:29:27 -0300

    feat:improve header responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,20 @@ function App(): JSX.Element {
       {/* Header */}
       <header className="bg-blue-950 text-white">
         <div className="container mx-auto px-4 py-2">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center space-x-2">
-              <Phone size={18} />
-              <span>(88) 99265-6171</span>
-              <Mail size={18} />
-              <span>contato@objetivarepresentacoes.com.br</span>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0">
+            <div className="flex flex-col sm:flex-col md:flex-row items-center md:items-start gap-2 md:gap-4">
+              <div className="flex items-center gap-2">
+                <Phone size={18} />
+                <span className="text-sm md:text-base">(88) 99265-6171</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <Mail size={18} />
+                <span className="text-sm md:text-base">contato@objetivarepresentacoes.com.br</span>
+              </div>
             </div>
-            <div className="flex items-center">
-              <MapPin size={18} className="mr-2" />
-              <span>Camocim, CE</span>
+            <div className="flex items-center gap-2">
+              <MapPin size={18} />
+              <span className="text-sm md:text-base">Camocim, CE</span>
             </div>
           </div>
         </div>
