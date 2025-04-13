7d31fe97bf62154f2e4767e57b8c11f39dcde854
AssFerj <assisjuniorcam@gmail.com>
2025-04-13 18:27:05 -0300

    update

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,10 +9,10 @@ export const brands: Brand[] = [
 ];
 
 export const products: Product[] = [
-  { id: 1, name: 'Detergente Multiuso', brand: 'Omo Professional', image: 'https://images.unsplash.com/photo-1585421514738-01798e348b17?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
-  { id: 2, name: 'Desinfetante', brand: 'Veja', image: 'https://images.unsplash.com/photo-1610557892470-55d587a6cd59?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
-  { id: 3, name: 'Papel Toalha', brand: 'Kimberly-Clark', image: 'https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
-  { id: 4, name: 'Sabonete Líquido', brand: 'Johnson', image: 'https://images.unsplash.com/photo-1622503174772-02f41da39b3d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
-  { id: 5, name: 'Alvejante', brand: 'Brilhante', image: 'https://images.unsplash.com/photo-1585421514738-01798e348b17?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
-  { id: 6, name: 'Limpador de Vidros', brand: 'Veja', image: 'https://images.unsplash.com/photo-1584735174914-6b1272458e3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 1, name: 'Detergente Multiuso', brand: 'Limpemax', image: 'https://images.unsplash.com/photo-1585421514738-01798e348b17?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 2, name: 'Desinfetante', brand: 'Liz Embalagens', image: 'https://images.unsplash.com/photo-1610557892470-55d587a6cd59?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 3, name: 'Papel Toalha', brand: 'Perfil Papéis', image: 'https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 4, name: 'Sabonete Líquido', brand: 'Petyan', image: 'https://images.unsplash.com/photo-1622503174772-02f41da39b3d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 5, name: 'Alvejante', brand: 'Cristal', image: 'https://images.unsplash.com/photo-1585421514738-01798e348b17?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
+  { id: 6, name: 'Limpador de Vidros', brand: 'Liz Embalagens', image: 'https://images.unsplash.com/photo-1584735174914-6b1272458e3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
 ];
