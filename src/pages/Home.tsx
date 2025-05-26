671fa83a0b8735845085877d9dc080549e3de9c0
AssFerj <assisjuniorcam@gmail.com>
2025-05-26 20:18:36 -0300

    feat:update email in contact section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -83,9 +83,9 @@ export default function Home() {
                                 <Phone size={18} />
                                 <span className="text-sm md:text-base">(88) 99254-5339</span>
                             </a>
-                            <a href="mailto:contato@objetivarepresentacoes.com.br" className="flex items-center gap-2 hover:text-blue-950 transition" target="_blank" rel="noopener noreferrer">
+                            <a href="mailto:objetivarepresentacoescam@gmail.com" className="flex items-center gap-2 hover:text-blue-950 transition" target="_blank" rel="noopener noreferrer">
                                 <Mail size={18} />
-                                <span className="text-sm md:text-base">contato@objetivarepresentacoes.com.br</span>
+                                <span className="text-sm md:text-base">objetivarepresentacoescam@gmail.com</span>
                             </a>
                             <div className="flex items-center gap-2">
                                 <MapPin size={18} />
